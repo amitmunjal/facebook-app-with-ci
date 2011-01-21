e93891ec367dd9746c7df97ef4788d1930e07117
amitmunjal <amitmunjal4186@yahoo.co.in>
2011-01-21 17:30:34 +0530

    integrated the functionality of previous and next using jquery and ajax

--- system/application/views/friends.php
+++ system/application/views/friends.php
@@ -1,16 +1,29 @@
 <?php
 $this->load->view('header');
-echo br();
-foreach($friends as $friend) {
-	
-	$imageAttr = array(
-          'src' => 'http://graph.facebook.com/' . $friend['id'] . '/picture?type=square',
-          'class' => 'tooltip',
-          'title' => $friend['name'],
-	);
-	echo img($imageAttr);
-	sleep(0.1);
-}
+$this->load->helper('form'); //load the form helper for the button(form_button)
+$this->load->helper('url'); //load this helper for the site_url function
 
+echo br(2);
+$friendCount = 0;
+	echo '<div id="friendsDiv">';
+		foreach($friends as $friend) {
+			
+			$imageAttr = array(
+				  'src' => $friend['pic_square'],
+				  'class' => 'tooltip',
+				  'title' => $friend['name'],
+			);
+			echo img($imageAttr);
+			sleep(.1);
+		}
+		echo br();
+		$data = array(
+			'name' => 'button',
+			'content' => 'Next',
+			'onclick' => 'objFriend.fetchFriends(\'' . site_url() . '/ajax/friends/fetchFriends\', 1);'
+		);
+		echo form_button($data);	
+	echo '</div>';
+echo '<script type="text/javascript">var objFriend = new friendClass();</script>';
 $this->load->view('footer');
 ?>
